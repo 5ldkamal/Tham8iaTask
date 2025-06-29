50791ba6d812aa1a1da38585ee677cb07231adb5
KhaledKamal <khaled.kamal@samesystem.com>
2025-06-29 18:29:23 +0300

    Initial Commit

---
+++ Tham8iaTask/Tham8iaTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tham8iaTaskApp.swift
+//  Tham8iaTask
+//
+//  Created by Khaled Kamal on 29/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tham8iaTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
